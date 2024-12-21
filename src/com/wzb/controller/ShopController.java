4f029d8e5b31e6e569ca87ae27fdff04ac6a82db
wzb <2482924813@qq.com>
2024-12-21 23:03:11 +0800

    pet and improve user

--- src/com/wzb/controller/ShopController.java
+++ src/com/wzb/controller/ShopController.java
@@ -1,12 +1,15 @@
 package com.wzb.controller;
 
 import com.wzb.bean.User;
+import com.wzb.service.ShopService;
+import com.wzb.service.ShopServiceImpl;
 import com.wzb.utils.menu.shop.ShopMenu;
 
 import java.util.Scanner;
 
 public class ShopController {
     private static final Scanner sc = new Scanner(System.in);
+    private static final ShopService shopService = new ShopServiceImpl();
 
     public static void shop(User user) {
         boolean flag = true;
@@ -15,15 +18,23 @@ public class ShopController {
             int choice = Integer.parseInt(sc.nextLine());
             switch (choice) {
                 case 1: {
-
+                    shopService.showAllPet();
                     break;
                 }
                 case 2: {
-
+                    if (shopService.addShopCar(user)) {
+                        System.out.println("添加购物车成功");
+                    } else {
+                        System.out.println("添加购物车失败");
+                    }
                     break;
                 }
                 case 3: {
-
+                    if (shopService.pay(user)) {
+                        System.out.println("支付成功");
+                    } else {
+                        System.out.println("支付失败");
+                    }
                     break;
                 }
                 case 0: {
