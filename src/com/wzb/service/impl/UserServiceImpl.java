67ceed916b523d61c7481e6785e8437badcd4896
wzb <2482924813@qq.com>
2024-12-26 16:29:38 +0800

    userService完善、添加注释

--- src/com/wzb/service/impl/UserServiceImpl.java
+++ src/com/wzb/service/impl/UserServiceImpl.java
@@ -14,22 +14,37 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 用户相关服务
+ */
 public class UserServiceImpl implements UserService {
     private final LoginService login = new LoginServiceImpl();
     private final PetService petService = new PetServiceImpl();
     private final UserDao userDao = new UserDaoImpl();
     private final Scanner sc = new Scanner(System.in);
 
+    /**
+     * 用户登录
+     * @return 登录成功之后的用户对象
+     */
     @Override
     public User userLogin() {
         return login.login();
     }
 
+    /**
+     * 用户注册
+     * @return 用户是否注册成功
+     */
     @Override
     public boolean userRegister() {
         return login.register();
     }
 
+    /**
+     * 用户个人信息展示
+     * @param user 当前登录的用户
+     */
     @Override
     public void showUser(User user) {
         System.out.println("当前登录用户信息为：");
@@ -43,9 +58,13 @@ public class UserServiceImpl implements UserService {
         }
         System.out.println("该用户的宠物有：" + petList);
         System.out.println("该用户的订单有：" + user.getOrderList());
-        System.out.println();
     }
 
+    /**
+     * 用户充值
+     * @param user 当前登录的用户
+     * @return 用户充值的结果
+     */
     @Override
     public boolean reCharge(User user) {
         System.out.println(user.getUsername() + "的账户余额是" + user.getMoney());
@@ -54,11 +73,17 @@ public class UserServiceImpl implements UserService {
         user.addMoney(money);
         System.out.println("充值成功");
         user.setUpdateTime(LocalDateTime.now());
-        System.out.println(user.getUsername() + "的账户余额是" + user.getMoney());
-        userDao.saveUpdatedUserById(user);  // 更新后立刻保存
+        // 只要用户信息发生变动，文件中的用户信息必须马上修改
+        userDao.saveUpdatedUserById(user);
+        System.out.println(user.getUsername() + "现在的账户余额是" + user.getMoney());
         return true;
     }
 
+    /**
+     * 修改用户密码
+     * @param user 当前登录的用户
+     * @return 修改密码是否成功
+     */
     @Override
     public boolean changePassword(User user) {
         System.out.println("修改密码，若非本人操作，请立即冻结账号");
@@ -95,6 +120,11 @@ public class UserServiceImpl implements UserService {
         }
     }
 
+    /**
+     * 修改地址
+     * @param user 当前登录的用户
+     * @return 修改地址是否成功
+     */
     @Override
     public boolean changeAddress(User user) {
         System.out.println("请输入用户的地址");
@@ -106,6 +136,11 @@ public class UserServiceImpl implements UserService {
         return true;
     }
 
+    /**
+     * 注销用户
+     * @param user 当前登录的用户
+     * @return 注销用户是否成功
+     */
     @Override
     public boolean deleteMyself(User user) {
         System.out.println("你确定要注销本账户吗？");
